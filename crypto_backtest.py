e79ad59a0d3d20dda4ab8cb01ac4fa7cc541f95c
ertosns <mohab-metwally@riseup.net>
2024-04-27 05:17:31 +0300

    remove yh-fin header

--- crypto_backtest.py
+++ crypto_backtest.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import yfinance as yf
 from scipy.optimize import minimize
 from Historic_Crypto import HistoricalData, Cryptocurrencies
 import threading
