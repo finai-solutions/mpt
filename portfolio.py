76eb344656ece8d3a0e845f5dd0f1816411a76b1
ertosns <mohab-metwally@riseup.net>
2024-05-05 23:52:49 +0300

    write returns to disk

--- portfolio.py
+++ portfolio.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 
 from scipy.optimize import minimize
+import json
 
 from strategies import equal_weight, minimum_variance, max_sharpe,  portfolio_return, portfolio_std, portfolio_sharpe
 from utils import get_write_path
@@ -23,7 +24,7 @@ def get_equally_weighted_portfolio(start_date, end_date, granularity, market_cap
         var_dict['return'] = equally_weighted_return
         var_dict['std'] = equally_weighted_std
         var_dict['sharpe'] = equally_weighted_sharpe_ratio
-        f_eq.write(str(var_dict))
+        f_eq.write(json.dumps(var_dict))
     return equally_weighted_weights, equally_weighted_std, equally_weighted_return
 
 def get_global_minimum_variance_portfolio(start_date, end_date, granularity, market_cap, bound, return_period, tickers, log_return, mean_return, cov):
@@ -41,7 +42,7 @@ def get_global_minimum_variance_portfolio(start_date, end_date, granularity, mar
         var_dict['return'] = gmv_return
         var_dict['std'] = gmv_std
         var_dict['sharpe'] = gmv_sharpe_ratio
-        f_var.write(str(var_dict))
+        f_var.write(json.dumps(var_dict))
     return gmv_weights, gmv_std, gmv_return
 
 def get_max_sharpe_portfolio(start_date, end_date, granularity, market_cap, bound, return_period, tickers, log_return, mean_return, cov):
@@ -58,7 +59,7 @@ def get_max_sharpe_portfolio(start_date, end_date, granularity, market_cap, boun
         var_dict['return'] = max_sharpe_return
         var_dict['std'] = max_sharpe_std
         var_dict['sharpe'] = max_sharpe_sharpe_ratio
-        f_sharpe.write(str(var_dict))
+        f_sharpe.write(json.dumps(var_dict))
     return max_sharpe_weights, max_sharpe_std, max_sharpe_return
 
 def get_portfolio(start_date, end_date, granularity, market_cap, bound, return_period, balance, verbose=True, singlecore=True):
@@ -68,8 +69,15 @@ def get_portfolio(start_date, end_date, granularity, market_cap, bound, return_p
         eq_w, eq_std, eq_return = get_equally_weighted_portfolio(start_date, end_date, granularity, market_cap, bound, return_period, tickers, mean_return, cov)
         gmv_w, gmv_std, gmv_return = get_global_minimum_variance_portfolio(start_date, end_date, granularity, market_cap, bound, return_period, tickers, log_return, mean_return, cov)
         max_sharpe_w, max_sharpe_std, max_sharpe_return = get_max_sharpe_portfolio(start_date, end_date, granularity, market_cap, bound, return_period, tickers, log_return, mean_return, cov)
+        with open(get_write_path(start_date, end_date, granularity, market_cap, bound, return_period, 'returns', ext='json'), 'w+') as f_returns:
+            returns_dict = {}
+            returns_dict['equally_weighted_return'] = eq_return
+            returns_dict['gmv_return'] = gmv_return
+            returns_dict['max_sharpe_return'] = max_sharpe_return
+            f_returns.write(json.dumps(returns_dict))
         plot(start_date, end_date, granularity, market_cap, bound, prices, tickers, balance, return_period, mean_return, cov, eq_return, eq_std, eq_w, gmv_return, gmv_std, gmv_w, max_sharpe_return, max_sharpe_std, max_sharpe_w)
         return True
     except Exception as e:
-        print(e)
+        if verbose:
+            print(e)
         return None
